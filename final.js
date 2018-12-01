34431b28b4210fee7083a666bc37309624d92e3f
Bethany Ebbitt <bebbitt@live.unc.edu>
2018-12-01 16:10:23 -0500

    Update final.js

--- final.js
+++ final.js
@@ -69,8 +69,13 @@ var build_airlines_interface = function() {
 
         let attext=$('<p class="attext">Enter an arrival location:</p>');
         body.append(attext);
-        let arrive_time=$('<input type="text" class="arrive_time"></input>');
-        attext.append(arrive_time);
+        let arrive_place=$('<input type="text" class="arrive_place"></input>');
+        attext.append(arrive_place);
+
+        let partyt=$('<p class="partyt">Number of people traveling in your party (including yourself):</p>');
+        body.append(partyt);
+        let party_num=$('<input type="text" class="party_num"></input>');
+        partyt.append(party_num);
 
         let submit_btn=$('<button id="submit_btn">Submit Information</button>');
         body.append(submit_btn);
@@ -78,15 +83,126 @@ var build_airlines_interface = function() {
     });
 
     $('body').on('click', '#submit_btn', function () {
-        let depart=$(this).parent().find('.depart_time').val();
+        console.log("submit");
+        let departt=$(this).parent().find('.depart_time').val();
+        let departp=$(this).parent().find('.depart_place').val();
+        console.log(departp);
+        let arrivep=$(this).parent().find('.arrive_place').val();
+        let partynum=$(this).parent().find('.party_num').val();
 
-        /*
-        let answerText = $(this).parent().find('.textBox').val();
+        //get list of airports
+                $.ajax(root_url+"airports/",
+                {
+                    type: 'GET',
+                     xhrFields: {withCredentials: true},
+                    success: (response) => {
+                        let depart = false;
+                        let did; let aid;
+                        let arrive = false;
+                        let airarray=response;
+                        for(let i=0;i<airarray.length;i++){
+                            let aircity=airarray[i].city;
+                            if(aircity.localeCompare(departp)==0){
+                                depart=true;
+                                did = airarray[i].id
+                            }
+                            if (aircity.localeCompare(arrivep)==0){
+                                arrive=true;
+                                aid = airarray[i].id
+                            }
+                        }
 
-        let ans=$(this).parent().find('.textBox');
-        */
+                        if(depart&&arrive){
+                            $.ajax(root_url+"flights",{
+                                type: 'GET',
+                                xhrFields: {withCredentials: true},
+                                success:(response)=>{
+                                    let farray=response;
+                                    for(let f=0; f<farray.length;f++){
+                                        let fdid = farray[f].departure_id;
+                                        let faid = farray[f].arrival_id;
+                                        if (fdid==did && faid==aid){
+                                            let pid = farray[f].plane_id;
+                                            for(let n=0; n<partynum;n++){
+                                                $.ajax(root_url+"seats",{
+                                                    type: 'POST',
+                                                    xhrFields:{withCredentials:true},
+                                                    data:{
+                                                     seat:{
+                                                            plane_id: pid,
+                                                            row: 1,
+                                                        number: "A",
+                                                        cabin: "economy",
+                                                        is_window:true
+                                                    }
+                                                }
+                                            });
+                                        }
+                                        for (let j=0; j<partynum;j++)
+                                        {
+                                            console.log("party");
+                                            let fnamet=$('<p class="fnamet">Enter your first name:</p>');
+                                            let fname=$('<input type="text" class="fname"><input>');
+                                            body.append(fnamet);
+                                            fnamet.append(fname);
+                                            
+                                            let lnamet=$('<p class="lnamet">Enter your last name:</p>');
+                                            let lname=$('<input type="text" class="lname"><input>');
+                                            body.append(lnamet);
+                                            lnamet.append(lname);
 
+
+
+
+
+                                           
+                                        }
+                                         return;
+                                        }
+                                    }
+                                }
+                            });
+                        }
+
+                    }
+                });
+
+        //getting requested flight 
+         $.ajax(root_url + "flights",
+           {
+           type: 'GET',
+           xhrFields: {withCredentials: true},
+           success: (response) => {
+               //console.log(response);
+               let farray=response;
+               //console.log(response);
+               for(let i=0; i<farray.length;i++){
+                let fid=farray[i].id;
+                let did=farray[i].departure_id;
+                let aid=farray[i].arrival_id;
+
+                
+
+
+
+
+               }
+
+
+
+
+
+
+
+
+           },
+           error: () => {
+               alert(error);
+           }
+           });
     });
+
+    
 
 
 
