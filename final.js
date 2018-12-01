546fd9c342be21677fdb0ded07f2b76029b9517a
Bethany Ebbitt <bebbitt@live.unc.edu>
2018-12-01 14:13:46 -0500

    udate js

--- final.js
+++ final.js
@@ -34,47 +34,9 @@ var build_airlines_interface = function() {
 
     body.empty();
 
-   /* body.append("<h2>Airlines</h2>");
+   
 
-    let airline_list = $("<ul id='airlines_list'></ul>");
-    body.append(airline_list);
-
-    let airline_add_div = $("<div>Name: <input id='new_airline_name' type='text'><br>" +
-			    "<button id='make_airline'>Create</button></div>");
-
-    body.append(airline_add_div);
-
-    
-    $.ajax(root_url + "airlines",
-	   {
-	       type: 'GET',
-	       xhrFields: {withCredentials: true},
-	       success: (airlines) => {
-		   for (let i=0; i<airlines.length; i++) {
-		       airline_list.append("<li>" + airlines[i].name + "</li>");
-		   }
-	       }
-	   });
-
-    $('#make_airline').on('click', () => {
-	let airline_name = $('#new_airline_name').val();
-
-	$.ajax(root_url + "airlines",
-	       {
-		   type: 'POST',
-		   data: {
-		       airline: {
-			   name: airline_name
-		       }
-		   },
-		   xhrFields: {withCredentials: true},
-		   success: (airline) => {
-		       airline_list.append("<li>" + airline.name + "</li>");
-		   }
-	       });
-    });*/
-
-    for(var i=0; i<3; i++){
+   /* for(var i=0; i<3; i++){
     	$.ajax(root_url + "flights",
 	       {
 		   type: 'POST',
@@ -93,7 +55,7 @@ var build_airlines_interface = function() {
 		   }
     	});
     };
-
+*/
 
 
 };
